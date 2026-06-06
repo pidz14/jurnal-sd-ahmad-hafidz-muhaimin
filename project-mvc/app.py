50caeb1da6b2d07d44294acf7c8e228da6cdda4c
ahmad hafidz muhaimin <2313020061@mhs.univ.ac.id>
2026-06-06 16:56:26 +0700

    [CHORE] integrasi awal frontend-backend secara local

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,33 +1,8 @@
-from views.dashboard_component import render_dashboard
-import time
-
-# Simulasi State
-app_state = {
-    "items": [],
-    "is_loading": True
-}
-
-def update_state(new_data):
-    app_state["items"] = new_data
-    app_state["is_loading"] = False
+from controllers.api_handler import get_users
+from views.dashboard_component import fetch_data_from_api, render_dashboard
 
 if __name__ == "__main__":
-
-    print("Loading data...")
-
-    # Kondisi loading
-    render_dashboard(app_state["items"], app_state["is_loading"])
-
-    time.sleep(2)
-
-    # Simulasi data dari backend
-    mock_data = [
-        {"id": 101, "name": "Produk A"},
-        {"id": 102, "name": "Produk B"}
-    ]
-
-    update_state(mock_data)
-
-    print("\nData berhasil dimuat:\n")
-
-    render_dashboard(app_state["items"], app_state["is_loading"])
+    # Proses Integrasi
+    data = fetch_data_from_api(get_users)
+    if data:
+        render_dashboard(data)
--- project-mvc/views/dashboard_component.py
+++ project-mvc/views/dashboard_component.py
@@ -1,12 +1,11 @@
-# views/dashboard_component.py
-
-def render_dashboard(data_list, is_loading):
-    print("--- DASHBOARD APLIKASI ---")
-
-    if is_loading:
-        print("Mohon Tunggu...")
-    elif not data_list:
-        print("[!] Data Kosong. Silakan sinkronisasi dengan Backend.")
-    else:
-        for item in data_list:
-            print(f"- Item ID: {item['id']} | Nama: {item['name']}")
+def fetch_data_from_api(api_function):
+    print("[System] Mencoba menghubungkan ke API...")
+    try:
+        response = api_function()
+        if response["status"] == "success":
+            return response["data"]
+        else:
+            raise Exception("API Return Error")
+    except Exception as e:
+        print(f"[Error] Gagal Integrasi: {e}")
+        return None

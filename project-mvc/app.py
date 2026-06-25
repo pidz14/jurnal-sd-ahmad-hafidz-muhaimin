94d6ec940f561201e89c959edeb44799109212dc
ahmad hafidz muhaimin <2313020061@mhs.univ.ac.id>
2026-06-25 08:50:41 +0700

    penambahan Dockerfile dan dockerignore

--- project-mvc/app.py
+++ project-mvc/app.py
@@ -1,4 +1,9 @@
-from controllers.user_controller import run_app
+import os
+
+# Membaca variabel lingkungan dengan nilai default (fallback)
+user_name = os.getenv('APP_USER', 'Guest')
+app_env = os.getenv('APP_ENV', 'development')
 
 if __name__ == "__main__":
-    run_app()
+    print(f"Halo {user_name}! Aplikasi ini berjalan di dalam kontainer Docker.")
+    print(f"Status Lingkungan: {app_env}")
